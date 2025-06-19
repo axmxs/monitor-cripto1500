7f7a1ce0964ef96eba87d9a77e4d430ee481c477
axmxs <lp.alan.lp@gmail.com>
2025-06-18 21:11:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-Commit new file
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "✅ Bot está rodando com sucesso!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def manter_online():
+    t = Thread(target=run)
+    t.start()
+
+manter_online()
+
+# Aqui você pode importar e rodar seu BOT normalmente
+import time
+
+while True:
+    print("⏱ Bot rodando...")
+    time.sleep(600)
