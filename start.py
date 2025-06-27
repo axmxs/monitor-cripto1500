cdd524d4415c9e488634c215df1de64e998dac5e
axmxs <lp.alan.lp@gmail.com>
2025-06-27 20:15:38 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,16 @@
 from threading import Thread
 import time
-import os
+import main
+import memebot
 
-# Roda o monitoramento da carteira pessoal (main.py)
 def iniciar_main():
-    os.system("python main.py")
+    main.iniciar()
 
-# Roda o Memebot para novas moedas (memebot.py)
 def iniciar_memebot():
-    os.system("python memebot.py")
+    memebot.iniciar()
 
 if __name__ == '__main__':
     print("🚀 Iniciando monitor de carteira e memebot...")
     Thread(target=iniciar_main).start()
-    time.sleep(3)  # pequeno delay para não colidir inicializações
+    time.sleep(3)
     Thread(target=iniciar_memebot).start()
