0678fa6590015d2913ea08945696d43f860eb271
axmxs <lp.alan.lp@gmail.com>
2025-07-01 11:21:23 -0300

    Update start.py

--- start.py
+++ start.py
@@ -2,10 +2,10 @@ from threading import Thread
 import time
 
 from main import iniciar_main
-from memebot_teste_loop_unico import main as iniciar_memebot  # CORRETO AGORA
+from memebot import iniciar_memebot
 
 if __name__ == '__main__':
-    print("🚀 Iniciando monitor de carteira e memebot (teste loop único)...")
+    print("🚀 Iniciando monitor de carteira e memebot...")
     Thread(target=iniciar_main).start()
     time.sleep(3)
     Thread(target=iniciar_memebot).start()
